8f59c873a3f501cac42d94188c19bb26bd80cac5
NeonN3mesis <129052650+NeonN3mesis@users.noreply.github.com>
2023-12-15 08:58:45 -0500

    Update wizard.sh
    Removed the error checking for bash -e
    added option for update with warning
    path is checked before export to prevent duplicate path

--- wizard.sh
+++ wizard.sh
@@ -1,58 +1,54 @@
-#!/bin/bash
-
-# Function to verify the success of the last command executed
-verify_success() {
-    if [ $? -ne 0 ]; then
-        echo "An error occurred. Exiting..."
-        exit 1
-    fi
-}
+#!/bin/bash -e
 
 echo "Starting setup process..."
 
-# Update and upgrade system
-sudo apt update && sudo apt upgrade -y
-verify_success
+# Optional system update with warning
+echo "Warning: Upgrading packages may affect other applications in non-fresh Linux/WSL environments."
+echo "It's recommended to update only in fresh Linux/WSL environments."
+read -p "Do you want to update and upgrade the system packages? (y/N): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo apt update
+    sudo apt upgrade
+fi
 
 # Install necessary dependencies
 sudo apt install apt-transport-https ca-certificates curl software-properties-common git python3 python3-pip -y
-verify_success
 
 # Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-verify_success
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-verify_success
 sudo apt update
 sudo apt install docker-ce -y
-verify_success
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $USER
-verify_success
 
-# Install Poetry and update PATH immediately for current session
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
+
+# Install Poetry
 curl -sSL https://install.python-poetry.org | python3 -
-verify_success
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-export PATH="$HOME/.local/bin:$PATH"
+
+# Check if Poetry path is already in PATH
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+    export PATH="$HOME/.local/bin:$PATH"
+fi
 
 # Clone and set up AutoGPT
 git clone https://github.com/Significant-Gravitas/AutoGPT.git
-verify_success
 cd AutoGPT/autogpts/autogpt
 ./setup
-verify_success
 
 # Prompt for OpenAI API key and update .env file
 echo "Enter your OpenAI API key:"
 read -r openai_api_key
 sed -i "s/your-openai-api-key/${openai_api_key}/" .env.template
 mv .env.template .env
-verify_success
 
-# Navigate to AutoGPT directory and run AutoGPT serve command
-cd AutoGPT/autogpts/autogpt
+# Run AutoGPT serve command
 ./autogpt.sh serve
 
 echo "AutoGPT server is running."
